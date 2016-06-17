2909428d60bc10daabd62500857b2f33f8f2e91c
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2016-06-17 22:31:39 +0100

    Update

--- publish.sh
+++ publish.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Build content and static files
 ./generate-for-production.sh
 
 # Copy files needed to deploy on GitHub Pages
 cp ./deploy/CNAME ./build/CNAME
 cp ./deploy/.nojekyll ./build/.nojekyll
 
-./update.sh
+# Commit and push
+git add .
+git commit -m "Update"
+git push
 
 # Deploy on GitHub Pages
 git subtree push --prefix build origin gh-pages
