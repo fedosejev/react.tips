190949d24fe94c19cb94fa729e03b14b0d83ed49
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2016-12-29 13:51:38 +0000

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,44 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
 
-gulp.task('build-js-for-development', function () {
-  return gulp
-        .src('./source/js/app.js')
-        .pipe(gulp.dest('./build/__static/js/'));
-});
+gulp.task('build-js-for-development', () => (
+  gulp
+    .src('./source/js/app.js')
+    .pipe(gulp.dest('./build/__static/js/'))
+));
 
-gulp.task('build-js-for-production', function () {
-  return gulp
-        .src('./source/js/app.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./build/__static/js/'));
-});
+gulp.task('build-js-for-production', () => (
+  gulp
+    .src('./source/js/app.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/__static/js/'))
+));
 
-gulp.task('build-sass-for-development', function () {
-  return gulp
-        .src('./source/sass/main.scss')
-        .pipe(sass())
-        .pipe(rename('styles.css'))
-        .pipe(gulp.dest('./build/__static/css/'));
-});
+gulp.task('build-sass-for-development', () => (
+  gulp
+    .src('./source/sass/index.scss')
+    .pipe(sass())
+    .pipe(rename('styles.css'))
+    .pipe(gulp.dest('./build/__static/css/'))
+));
 
-gulp.task('build-sass-for-production', function () {
-  return gulp
-        .src('./source/sass/main.scss')
-        .pipe(sass())
-        .pipe(rename('styles.css'))
-        .pipe(gulp.dest('./build/__static/css/'));
-});
+gulp.task('build-sass-for-production', () => (
+  gulp
+    .src('./source/sass/index.scss')
+    .pipe(sass())
+    .pipe(rename('styles.css'))
+    .pipe(gulp.dest('./build/__static/css/'))
+));
 
-gulp.task('build-images-for-production', function () {
-  return gulp
-        .src('./source/images/*.*')
-        .pipe(gulp.dest('./build/__static/images/'));
-});
+gulp.task('build-images-for-production', () => (
+  gulp
+    .src('./source/images/*.*')
+    .pipe(gulp.dest('./build/__static/images/'))
+));
 
-gulp.task('watch-for-development', function () {
+gulp.task('watch-for-development', () => {
   gulp.watch('./source/js/**/*.js', ['build-js-for-development']);
   gulp.watch('./source/sass/**/*.scss', ['build-sass-for-development']);
   gulp.watch('./source/images/**/*.*', ['build-images-for-production']);
