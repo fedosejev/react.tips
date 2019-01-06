7f0dcb2500d60f9e463942d657815e85afdf3ccb
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2019-01-06 18:36:21 +0000

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -38,22 +38,29 @@ gulp.task("build-images-for-production", () =>
     .pipe(gulp.dest(`./${BUILD_DIR}/__static/images/`))
 );
 
+gulp.task("build-static-for-production", () =>
+  gulp.src("./source/static/*.*").pipe(gulp.dest(`./${BUILD_DIR}/__static/`))
+);
+
 gulp.task("watch-for-development", () => {
   gulp.watch("./source/js/**/*.js", ["build-js-for-development"]);
   gulp.watch("./source/sass/**/*.scss", ["build-sass-for-development"]);
   gulp.watch("./source/images/**/*.*", ["build-images-for-production"]);
+  gulp.watch("./source/static/**/*.*", ["build-static-for-production"]);
 });
 
 gulp.task("build-for-development", [
   "build-js-for-development",
   "build-sass-for-development",
-  "build-images-for-production"
+  "build-images-for-production",
+  "build-static-for-production"
 ]);
 
 gulp.task("build-for-production", [
   "build-js-for-production",
   "build-sass-for-production",
-  "build-images-for-production"
+  "build-images-for-production",
+  "build-static-for-production"
 ]);
 
 gulp.task("default", ["build-for-development"]);
