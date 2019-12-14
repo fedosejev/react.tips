499b421db303ec33265568d32c10460a3e4c0c11
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2019-12-14 01:09:34 +0000

    Update

--- create-sitemap.js
+++ create-sitemap.js
@@ -1,16 +1,15 @@
-const fs = require('fs');
-const path = require('path');
-const sitemap = require('sitemap');
-const BUILD_DIR = require('./config').BUILD_DIR;
-const URL = require('./config').WEBSITE_URL;
+const fs = require("fs");
+const path = require("path");
+const sitemap = require("sitemap");
+const BUILD_DIR = require("./build-config").BUILD_DIR;
+const URL = require("./build-config").WEBSITE_URL;
 
 const getListOfDirectoriesInDataDirectory = () => {
-  const dataDirectoryPath = path.join(__dirname, 'data');
-  const fileNames = fs.readdirSync(dataDirectoryPath, 'utf8');
-  const directoryNames = fileNames
-  .filter(fileName => (
+  const dataDirectoryPath = path.join(__dirname, "data");
+  const fileNames = fs.readdirSync(dataDirectoryPath, "utf8");
+  const directoryNames = fileNames.filter(fileName =>
     fs.statSync(path.join(dataDirectoryPath, fileName)).isDirectory()
-  ));
+  );
 
   return directoryNames;
 };
@@ -19,29 +18,27 @@ const generateSitemapFile = () => {
   const outputFile = path.join(__dirname, `./${BUILD_DIR}/sitemap.xml`);
   const tutorials = getListOfDirectoriesInDataDirectory();
   const rootUrl = {
-    url: '/',
-    changefreq: 'weekly',
+    url: "/",
+    changefreq: "weekly",
     priority: 0.8,
     lastmodrealtime: true,
-    lastmodfile: path.join(__dirname, `/${BUILD_DIR}/index.html`),
+    lastmodfile: path.join(__dirname, `/${BUILD_DIR}/index.html`)
   };
 
-  const urls = tutorials.map(tutorial => (
-    {
-      url: `/${tutorial}`,
-      changefreq: 'monthly',
-      priority: 0.8,
-      lastmodrealtime: true,
-      lastmodfile: path.join(__dirname, `/${BUILD_DIR}/${tutorial}/index.html`),
-    }
-  ));
+  const urls = tutorials.map(tutorial => ({
+    url: `/${tutorial}`,
+    changefreq: "monthly",
+    priority: 0.8,
+    lastmodrealtime: true,
+    lastmodfile: path.join(__dirname, `/${BUILD_DIR}/${tutorial}/index.html`)
+  }));
 
   urls.unshift(rootUrl);
 
   const theSitemap = sitemap.createSitemap({
     hostname: URL,
     cacheTime: 600000, // 600 sec (10 min) cache purge period
-    urls,
+    urls
   });
 
   fs.writeFileSync(outputFile, theSitemap.toString());
@@ -60,7 +57,7 @@ const generate = () => {
 };
 
 module.exports = {
-  generate,
+  generate
 };
 
 generate();
