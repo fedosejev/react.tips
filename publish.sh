d11f4d3bf34c50ecf22c2f4a2f272aa12942c257
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2019-12-14 00:48:45 +0000

    Update

--- publish.sh
+++ publish.sh
@@ -7,7 +7,12 @@
 cp ./deploy/CNAME ./docs/CNAME
 cp ./deploy/.nojekyll ./docs/.nojekyll
 
+# Set user
+git config user.name "Artemij Fedosejev"
+git config user.email "artemij.fedosejev@gmail.com"
+
 # Commit and push
+git checkout master
 git add .
 git commit -m "Update"
 git push
