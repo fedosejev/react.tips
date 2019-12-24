e0b5b8145a9f716728d10f230cd6e0e255f50c79
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2019-12-24 02:03:58 +0000

    Update

--- publish.sh
+++ publish.sh
@@ -7,6 +7,9 @@
 cp ./deploy/CNAME ./docs/CNAME
 cp ./deploy/.nojekyll ./docs/.nojekyll
 
+# Copy Google AdSense file
+cp ./deploy/ads.txt ./docs/ads.txt
+
 # Set user
 git config user.name "Artemij Fedosejev"
 git config user.email "artemij.fedosejev@gmail.com"
